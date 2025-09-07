acfecf1aafea74e913168cca19e53896ddcf852a
Adam Atbi <adamatbi@gmail.com>
2025-09-07 12:53:51 -0700

    test using linspace to ramp amplitude

--- play_wav.py
+++ play_wav.py
@@ -26,6 +26,7 @@ import threading
 import shared
 import sounddevice as sd
 import soundfile as sf
+import numpy as np
 
 
 def int_or_str(text):
@@ -35,35 +36,49 @@ def int_or_str(text):
     except ValueError:
         return text
 
+prev_amp = 0.0
+
 def play_wav():
     event = threading.Event()
 
+        
     runs = 0
     try:
         gmajor_data, gmajor_fs = sf.read("audio/Gmajor.wav", always_2d=True)
         emajor_data, emajor_fs = sf.read("audio/Emajor.wav", always_2d=True)
-        global current_frame
+        global current_frame, amplitude_index
+        amplitude_index = 0
         current_frame = 0
 
         def callback(outdata, frames, time, status):
-            global current_frame
+            global current_frame, amplitude_index, prev_amp 
+            
             if status:
                 print(status)   # keep if you need diagnostics
             frames = int(frames)
             tail = len(gmajor_data) - current_frame
 
-            left_hand_amplitude = shared.scale_value_to_range(shared.left_hand_height,0.2,0.85) if shared.tracking else 0
-            right_hand_amplitude = shared.scale_value_to_range(shared.right_hand_height,0.2,0.85) if shared.tracking else 0
+            left_hand_amplitude = shared.scale_value_to_range(shared.left_hand_height,0,0.85) if shared.tracking else 0
+            right_hand_amplitude = shared.scale_value_to_range(shared.right_hand_height,0,0.85) if shared.tracking else 0
+            
+            gmajor_amplitude = (1-shared.right_hand_height) * shared.left_hand_height
+            emajor_amplitude = shared.right_hand_height * shared.left_hand_height
+
+
             
 
             if tail >= frames:
-                combined_data = (gmajor_data[current_frame:current_frame + frames] * right_hand_amplitude +
-                                 emajor_data[current_frame:current_frame + frames] * left_hand_amplitude)
+                ramp = np.linspace(prev_amp, right_hand_amplitude, frames).reshape(-1, 1)
+
+                combined_data = gmajor_data[current_frame:current_frame + frames] * ramp
+                prev_amp = right_hand_amplitude
+                #(gmajor_data[current_frame:current_frame + frames] * left_hand_amplitude +
+                                #emajor_data[current_frame:current_frame + frames] * right_hand_amplitude)
                 outdata[:] = combined_data
                 
                 current_frame += frames
             else:
-                1/0
+                pass
                 # outdata[:tail] = data[current_frame:] * amplitude
                 # outdata[tail:frames] = data[:frames - tail] * amplitude
                 # current_frame = frames - tail
